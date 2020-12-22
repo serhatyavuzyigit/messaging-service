133c2e31b602c00bef691ead2ee91414bbef4835
serhatyavuzyiit <soyavuzyigit@gmail.com>
2020-12-22 19:31:11 +0300

    controllers codes reorganized

--- api/controllers/blocks.js
+++ api/controllers/blocks.js
@@ -1,42 +1,51 @@
 const User = require('../models/user');
 
 exports.blocks_add_blocks = (req, res, next) => {
+    var checkFlag = true;
+    const userData = req.userData;
     const blockFrom = req.body.blockFrom;
     const blockTo = req.body.blockTo;
-    User.find({ username: blockFrom })
-        .exec()
-        .then(user => {
-            if (user[0].blocks.indexOf(blockTo) === -1) {
-                user[0].blocks.push(blockTo);
-                user[0].save();
-            } else {
-                return res.status(200).json({
-                    message: "They are already blocked each other"
-                });
-            }
-        })
-        .catch(err => {
-            res.status(500).json({ error: err });
-        });
-    
-    User.find({ username: blockTo })
-        .exec()
-        .then(user => {
-            if (user[0].blocks.indexOf(blockFrom) === -1) {
-                user[0].blocks.push(blockFrom);
-                user[0].save();
-            } else {
-                return res.status(200).json({
-                    message: "They are already blocked each other"
-                });
-            }
-        })
-        .catch(err => {
-            res.status(500).json({ error: err });
-        });
-    return res.status(201).json({
-        message: "They blocked each other"
-    });
+    if (userData.username !== blockFrom) {
+        checkFlag = false;
+    }
 
-    
+    if(checkFlag) {
+        User.find({ username: blockTo })
+        .exec()
+        .then(usr => {
+            if (usr.length === 0) {
+                res.status(500).json({
+                    message: blockTo  + " is not a valid username"
+                });    
+            } else {
+                User.find({ username: blockFrom })
+                .exec()
+                .then(user => {
+                    if (user[0].blocks.indexOf(blockTo) === -1) {
+                        user[0].blocks.push(blockTo);
+                        user[0].save();
+                        res.status(201).json({
+                            message: blockFrom + " added " + blockTo + " to his-her blocks list"
+                        });
+                    } else {
+                        res.status(200).json({
+                            message: blockFrom + " already added " + blockTo + " to his-her blocks list"
+                        });
+                    }
+                })
+                .catch(err => {
+                    console.log(err);
+                    res.status(500).json({ error: err });
+                });
+                    }
+                })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: err });
+        });    
+    } else {
+        res.status(500).json({
+            message: "Given token is not associated with the user"
+        });
+    }
 };
--- api/controllers/friends.js
+++ api/controllers/friends.js
@@ -1,42 +1,53 @@
+const { compare } = require('bcrypt');
 const User = require('../models/user');
 
 exports.friends_add_friends = (req, res, next) => {
+    var checkFlag = true;
+    const userData = req.userData;
     const friendFrom = req.body.friendFrom;
     const friendTo = req.body.friendTo;
-    User.find({ username: friendFrom })
-        .exec()
-        .then(user => {
-            if (user[0].friends.indexOf(friendTo) === -1) {
-                user[0].friends.push(friendTo);
-                user[0].save();
-            } else {
-                return res.status(200).json({
-                    message: "They are already friends"
-                });
-            }
-        })
-        .catch(err => {
-            res.status(500).json({ error: err });
-        });
-    
-    User.find({ username: friendTo })
-        .exec()
-        .then(user => {
-            if (user[0].friends.indexOf(friendFrom) === -1) {
-                user[0].friends.push(friendFrom);
-                user[0].save();
-            } else {
-                return res.status(200).json({
-                    message: "They are already friends"
-                });
-            }
-        })
-        .catch(err => {
-            res.status(500).json({ error: err });
-        });
-    return res.status(201).json({
-        message: "Friendship began"
-    });
+    if (userData.username !== friendFrom) {
+        checkFlag = false;
+    }
 
+    if(checkFlag) {
+        User.find({ username: friendTo })
+        .exec()
+        .then(usr => {
+            if (usr.length === 0) {
+                res.status(500).json({
+                    message: friendTo + " is not a valid username"
+                });    
+            } else {
+                User.find({ username: friendFrom })
+                .exec()
+                .then(user => {
+                    if (user[0].friends.indexOf(friendTo) === -1) {
+                        user[0].friends.push(friendTo);
+                        user[0].save();
+                        res.status(201).json({
+                            message: friendFrom + " added " + friendTo + " to his-her friends list"
+                        });
+                    } else {
+                        res.status(200).json({
+                            message: friendFrom + " already added " + friendTo + " to his-her friends list"
+                        });
+                    }
+                })
+                .catch(err => {
+                    console.log(err);
+                    res.status(500).json({ error: err });
+                });
+                    }
+                })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: err });
+        });    
+    } else {
+        res.status(500).json({
+            message: "Given token is not associated with the user"
+        });
+    }
     
 };
